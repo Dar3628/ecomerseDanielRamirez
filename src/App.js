28024dc169f8c33fed7aa8fd6cdfe1648ed9fc77
Daniel Ramirez <daniel_ramirez89@hotmail.com>
2022-03-16 22:11:14 -0300

    peleando pa que ande

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   return (
+    <div className = "App">
+      <Navbar />
+      <h1>Error: {"errorMessage"}</h1>
+      <h1> Primer intento con React </h1>
+      <firstReactNavbar/>
+      <img src='loguitoPng'>loguito</img>
     </div>
   );
 }
---
+++ src/icon/CartIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import loguitoPng from './loguito.png'
+
+
+const CartIcon = () => {
+    return  <img className='CartIcon' src={loguitoPng} alt='loguitoPng' />;
+}
+
+export default CartIcon

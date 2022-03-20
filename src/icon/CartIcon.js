b62b3c9286dfed0ffa25420d739252573fb2baa5
Daniel Ramirez <daniel_ramirez89@hotmail.com>
2022-03-20 04:37:16 -0300

    se agrega itemListContainer

--- src/icon/CartIcon.js
+++ src/icon/CartIcon.js
@@ -3,7 +3,8 @@ import loguitoPng from './loguito.png'
 
 
 const CartIcon = () => {
-    return  <img className='CartIcon' src={loguitoPng} alt='loguitoPng' />;
+    return  <img className='CartIcon' src={loguitoPng} alt='loguitoPng' height={62} width={82} />
+    
 }
 
 export default CartIcon
